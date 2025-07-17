ffd1858b740882f1479328a3d108a12e3b5adca2
JP Grineau <jgrineau95@gmail.com>
2025-07-16 22:18:46 -0400

    png file updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,23 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Visual Timers",
+  openGraph: {
+    title: "Visual Timers",
+    description:
+      "Visual Timers is a Next.js app offering customizable Pomodoro, radial, and linear timers. Perfect for productivity, focus, and time management. Try our visual countdowns and explore timer tools for work, study, and more.",
+    url: "https://visualtimers.net",
+    siteName: "Visual Timers",
+    images: [
+      {
+        url: "/.next/static/media/favicon.8be4e9dc.ico", // Replace with your actual image path
+        width: 1200,
+        height: 630,
+        alt: "Visual Timers - Pomodoro, Radial, and Linear Timers",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
   keywords: [
     "timers",
     "visual timers",
